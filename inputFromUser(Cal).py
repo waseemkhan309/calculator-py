cfda7a6d2e6e66a892cb247a6ba183c2f265cc14
WASEEM KHAN <113575656+waseemkhan309@users.noreply.github.com>
2022-12-09 16:12:10 +0500

    Add files via upload

---
+++ inputFromUser(Cal).py
@@ -0,0 +1,15 @@
+'''
+Create Calculator that is capable of All Arithmetic Operations between two numbers taking from user.  
+'''
+
+a=input("Enter 1st Number = ")
+b=input("Enter 2nd Number = ")
+# Arithmetics Operation
+print("SUM of ",a," and ",b ," = ",int(a)+int(b) )      # Addition
+print("SUB of ",a," and ",b ," = ",int(a)-int(b) )      # Subtraction
+print("MUL of ",a," and ",b ," = ",int(a)*int(b) )      # Multiplication
+print("DIV of ",a," and ",b ," = ",int(a)/int(b) )      # Division
+print("MOD of ",a," and ",b ," = ",int(a)%int(b) )      # Modulus
+print("Floor DIV of ",a," and ",b ," = ",int(a)//int(b) )     # Floor Division
+print("Exponential of ",a," and ",b ," = ",int(a)**int(b) )     # Exp0nential
+print("Arithmetic Operations Done...")
