b90db656561f914eb64b97c84108b741e3702ef7
WASEEM KHAN <113575656+waseemkhan309@users.noreply.github.com>
2022-12-09 12:15:51 +0500

    Add files via upload

---
+++ calculatorByPython.py
@@ -0,0 +1,13 @@
+"""
+create a calculator capable of performing arithmetic operations on two number.
+"""
+num1,num2=10,20
+print("1st value is ",num1)
+print("2nd value is ",num2)
+print("Sum(10 + 20) = ",num1+num2)
+print("Subtract(10 - 20) = ",num1-num2)
+print("Mul(10 * 20) = ",num1*num2)
+print("Div(10 / 20) = ",num1/num2)
+print("Floor Div(10 // 20) = ",num1//num2)
+print("Exponential(10 ** 20) = ",num1**num2)
+print("Modulus(10 % 20) = ",num1%num2)
